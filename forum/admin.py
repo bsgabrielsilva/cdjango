afa179b10195b256e4409ec3374e8b3fc7778482
Gabriel Silva <martinsgpgabriel@gmail.com>
2020-03-13 14:43:55 -0300

    Inserção dos Models de cada Módulo no Painel ADMIN do Django

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Topicos, Respostas
+
 
 # Register your models here.
+admin.site.register(Topicos)
+admin.site.register(Respostas)
--- questoes/admin.py
+++ questoes/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Niveis, Perguntas, Simulados
 
 # Register your models here.
+admin.site.register(Niveis)
+admin.site.register(Perguntas)
+admin.site.register(Simulados)
